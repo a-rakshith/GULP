db47c433105c1e1b8575555cde9cca1f8bd09d0e
a-rakshith <rakshith.pes@gmail.com>
2016-03-18 15:50:02 +0530

    Added files via upload

---
+++ app/js/main.js
@@ -0,0 +1,10 @@
+//this is a example
+
+
+	console.log("hello first");
+	console.log("hello to js!!");
+	console.log("hello world!!!!");
+	console.log("hello second");
+	console.log("hello this is GULP");
+
+	document.getElementById("sweet-header").innerHTML="WORKING!!!!!!";
